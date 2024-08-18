7bdc19c0e74ebff88040225cea3491af1d8b16b6
ashi-saf <152131562+ashi-saf@users.noreply.github.com>
2024-08-18 21:43:13 +0530

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //place user order from frontend
 const placeOrder = async (req, res) => {
-  const frontEndUrl = 'http://localhost:5173';
+  const frontEndUrl = "https://foodorderingapp-frontend-1hgv.onrender.com/";
   const { userId, items, amount, address, paymentMethod } = req.body;
   try {
     if (!paymentMethod) {
